10a53b93de00fb8192daf386e237d747e15051d6
LukedeArmas <luke.p.dearmas@vanderbilt.edu>
2023-08-16 22:16:07 -0400

    fix dotenv

--- backend/src/startup/init.ts
+++ backend/src/startup/init.ts
@@ -1,19 +1,16 @@
-import { Express } from 'express'
-import dotenv from 'dotenv';
+import { Express } from 'express';
+import 'dotenv/config';
 import createServer from './server';
 
 
 export default async function init() {
-  dotenv.config();
-  const PORT = process.env.PORT;
-
   // Connect to DB here
 
   console.log('Starting server');
   const app: Express = createServer();
 
-  app.listen(PORT, () => {
-    console.log(`Server started on PORT ${PORT}`);
+  app.listen(process.env.PORT, () => {
+    console.log(`Server started on PORT ${process.env.PORT}`);
   })
 
 }
